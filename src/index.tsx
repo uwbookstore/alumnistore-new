5e60547d0d6f2ffb7c7d9f9b9980870d29440274
ClaytonDewey <clay@dryadmedia.com>
2026-01-10 14:43:25 -0600

    chore: Install router-dom & add pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import 'react-circular-progressbar/dist/styles.css';
 import './sass/style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import { Main } from '../components';
+const Home = () => {
+  return <Main />;
+};
+export default Home;
