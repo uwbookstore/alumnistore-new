82c086e51e5223196a8e96c3346b60020a5850b5
ClaytonDewey <clay@dryadmedia.com>
2026-05-06 14:47:53 -0500

    refactor: Add search overlay on mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,34 +1,34 @@
-// const searchOverlay = document.getElementById('search-overlay');
-// const openSearch = document.getElementById('open-search');
-// const closeSearch = document.getElementById('close-search');
+const searchOverlay = document.getElementById('search-overlay');
+const openSearch = document.getElementById('open-search');
+const closeSearch = document.getElementById('close-search');
 
-// const openSearchOverlay = () => {
-//   searchOverlay.classList.add('open');
-// };
+const openSearchOverlay = () => {
+  searchOverlay.classList.add('open');
+};
 
-// const closeSearchOverlay = () => {
-//   searchOverlay.classList.remove('open');
-// };
+const closeSearchOverlay = () => {
+  searchOverlay.classList.remove('open');
+};
 
-// openSearch.addEventListener('click', () => {
-//   openSearchOverlay();
-// });
+openSearch.addEventListener('click', () => {
+  openSearchOverlay();
+});
 
-// closeSearch.addEventListener('click', () => {
-//   closeSearchOverlay();
-// });
+closeSearch.addEventListener('click', () => {
+  closeSearchOverlay();
+});
 
-// searchOverlay.addEventListener('click', (e) => {
-//   if (e.target === searchOverlay) {
-//     closeSearchOverlay();
-//   }
-// });
+searchOverlay.addEventListener('click', (e) => {
+  if (e.target === searchOverlay) {
+    closeSearchOverlay();
+  }
+});
 
-// document.addEventListener('keydown', (e) => {
-//   if (e.key === 'Escape') {
-//     closeSearchOverlay();
-//   }
-// });
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeSearchOverlay();
+  }
+});
 
 // Navbar Toggle
 const navbarToggle = document.getElementById('nav-toggle');
