b9b42653a9694476708fababffa45d9a528e2380
ClaytonDewey <clay@dryadmedia.com>
2026-05-13 14:55:00 -0500

    refactor: Reorganize media queries

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,50 @@
-const searchOverlay = document.getElementById('search-overlay');
-const openSearch = document.getElementById('open-search');
-const closeSearch = document.getElementById('close-search');
+// const searchOverlay = document.getElementById('search-overlay');
+// const openSearch = document.getElementById('open-search');
+// const closeSearch = document.getElementById('close-search');
 
-const openSearchOverlay = () => {
-  searchOverlay.classList.add('open');
-};
+// const openSearchOverlay = () => {
+//   searchOverlay.classList.add('open');
+// };
 
-const closeSearchOverlay = () => {
-  searchOverlay.classList.remove('open');
-};
+// const closeSearchOverlay = () => {
+//   searchOverlay.classList.remove('open');
+// };
 
-openSearch.addEventListener('click', () => {
-  openSearchOverlay();
-});
+// openSearch.addEventListener('click', () => {
+//   openSearchOverlay();
+// });
 
-closeSearch.addEventListener('click', () => {
-  closeSearchOverlay();
-});
+// closeSearch.addEventListener('click', () => {
+//   closeSearchOverlay();
+// });
 
-searchOverlay.addEventListener('click', (e) => {
-  if (e.target === searchOverlay) {
-    closeSearchOverlay();
-  }
-});
+// searchOverlay.addEventListener('click', (e) => {
+//   if (e.target === searchOverlay) {
+//     closeSearchOverlay();
+//   }
+// });
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    closeSearchOverlay();
-  }
-});
+// document.addEventListener('keydown', (e) => {
+//   if (e.key === 'Escape') {
+//     closeSearchOverlay();
+//   }
+// });
 
-// Navbar Toggle
-const navbarToggle = document.getElementById('nav-toggle');
-const primaryNav = document.getElementById('waa-nav');
+// // Navbar Toggle
+// const navbarToggle = document.getElementById('nav-toggle');
+// const primaryNav = document.getElementById('waa-nav');
+
+// navbarToggle.addEventListener('click', () => {
+//   primaryNav.classList.toggle('open');
+// });
+
+const navbarToggle = document.querySelector('[data-bs-toggle="collapse"]');
+const toggleIcon = document.querySelector('[data-bs-toggle="collapse"] img');
 
 navbarToggle.addEventListener('click', () => {
-  primaryNav.classList.toggle('open');
+  navbarToggle.classList.contains('collapsed')
+    ? toggleIcon.setAttribute('src', './img/OS_icons_menu.png')
+    : toggleIcon.setAttribute('src', './img/OS_icons_close.png');
 });
 
 // Footer copyright info
